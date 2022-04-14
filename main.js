21ca7c32b1600099073605a95a90f588d37fd793
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-04-13 23:20:20 -0400

    added age api

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.querySelector('#btn').addEventListener('click', GetName)
-document.querySelector('#btn').addEventListener('click', GetAge)
 
-function GetName() {
+
+async function GetName() {
     let name = document.querySelector('.input').value
 
     fetch(`https://api.genderize.io?name=${name}`)
@@ -11,44 +11,40 @@ function GetName() {
 
         document.querySelector('.name').innerText = `Name : ${name}.`
         document.querySelector('.gender').innerText = `Gender : ${data.gender}.`
-        // document.querySelector('.probability').innerText = `Probability : ${data.probability*100}%.`
         document.querySelector('.count').innerText = `Sample size : ${data.count} people named ${data.name} found.`
-        // document.querySelector('.summary').innerText = `Summary :
-        // Based on a sample size of ${data.count} people named ${data.name}, we have calculated that the probability of ${data.name} being a ${data.gender} is ${data.probability*100}%.`
+      })
 
+      
+    //      todo 
+    //      get nationalize working 
+      
+        fetch(`https://api.nationalize.io/?name=${name}`)
+        .then( res => res.json())
+        .then( data2 => {
+            console.log(data2)
+            
+            document.querySelector('.country1').innerText = `country1 : ${data2.country}.`                    
+            document.querySelector('.country2').innerText = `country2 : ${country[2].country_id}.`           
+            document.querySelector('.country3').innerText = `country3 : ${country_id[2]}.`           
+
+
+})
         fetch(`https://api.agify.io/?name=${name}`)
         .then( res => res.json())
-        .then( data1 => {
-            console.log(data1)
+        .then( data3 => {
+            console.log(data3)
     
-            document.querySelector('.age').innerText = `Age : ${data1.age}.`           
-            
-            document.querySelector('.summary').innerText = `Summary :
+            document.querySelector('.age').innerText = `Age : ${data3.age}.`           
+                        
+})   
+
+ document.querySelector('.summary').innerText = `Summary :
             Out of ${data.count} people named ${data.name}, ${data.probability*100}% were ${data.gender}.
-            Among the ${data.count} we found the most common age to be ${data1.age}.` 
-    })
+            Among the ${data.count} we found the most common age to be ${data3.age}.`
+
+
     .catch(err => {
         console.log(`error ${err}`)
     });
-
-    // function GetAge() {
-    //     let name = document.querySelector('.input').value
-    
-    //     fetch(`https://api.agify.io/?name=${name}`)
-    //     .then( res => res.json())
-    //     .then( data => {
-    //         console.log(data)
-    
-    //         document.querySelector('.age').innerText = `Age : ${data.age}.`           
-    //         document.querySelector('.probability').innerText = `Probability : ${data.probability*100}%.`
-    //         document.querySelector('.count').innerText = `Sample size : ${data.count} people named ${data.name} found.`
-    //         document.querySelector('.summary').innerText = `Summary :
-    //         Based on a sample size of ${data.count} people named ${data.name}, we have calculated that the probability of ${data.name} being ${data.age} years of age is ${data.probability*100}%.`
-    
-    //     })
-    //     .catch(err => {
-    //         console.log(`error ${err}`)
-    //     });
-
-})
+        
 }
